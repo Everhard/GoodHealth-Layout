16e0faeec2da259b97d73bc2e7598e2cb9b6a090
Andrew Dorokhov <andrew@dorohoff.net>
2016-02-18 00:07:37 +0200

    Add header HTML and base styles.

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,11 +9,18 @@ module.exports = function(grunt) {
         },
         
         copy: {
-            files: {
+            main: {
+                files: [{
                 cwd: "bower_components/normalize-css",
                 src: "normalize.css",
                 dest: "dist/css",
                 expand: true
+                }, {
+                    cwd: "src",
+                    src: "img/*",
+                    dest: "dist",
+                    expand: true
+                }]
             }
         },
         
@@ -23,12 +30,23 @@ module.exports = function(grunt) {
                     "dist/css/styles.css" : "src/less/styles.less"
                 }
             }
-        }
+        },
+        
+        watch: {
+            scripts: {
+              files: ['src/**/*'],
+              tasks: ['default'],
+              options: {
+                spawn: true,
+              },
+            },
+        },
     });
     
     grunt.loadNpmTasks('grunt-rigger');
     grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-watch');
     
     grunt.registerTask('default', ['rig', 'copy', 'less']);
 };
---
+++ README.md
@@ -0,0 +1,5 @@
+[![Build Status](https://travis-ci.org/Everhard/GoodHealth-Layout.svg?branch=master)](https://travis-ci.org/Everhard/GoodHealth-Layout)
+
+# GoodHealth Layout
+
+Frontend layout for GoodHealth online store.
